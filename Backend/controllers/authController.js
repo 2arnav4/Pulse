55bf72fd743eee36b3bda8ceabe061c6fa873d2d
2arnav4 <singlaarnav2405@gmail.com>
2026-02-17 17:37:35 +0530

    Authentication & routes

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,83 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+// 1. REGISTER USER
+exports.register = async (req, res) => {
+    try {
+        //Get Data from the frontend request
+        const { username, email, password } = req.body;
+
+        // Check if user already exists
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        //Hash the password (encrypt it)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create the new user in the database 
+        const newUser = await User.create({
+            username,
+            email,
+            password_hash: hashedPassword,
+        });
+        // Create a Token (JWT) so that user is logged in immediately 
+        const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+            expiresIn: '1d',
+        });
+
+        //Send success response 
+        res.status(201).json({
+            message: 'User registered successfully',
+            token,
+            user: {
+                id: newUser.id,
+                username: newUser.username,
+                email: newUser.email,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+// 2. LOGIN USER 
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Find user by email
+        const user = await User.findOne({ where: { email } })
+        if (!user) {
+            return res.status(404).json({ message: 'Invalid credentials ' });
+        }
+
+        // Compare the password they typed with the hashed password in the DB
+        const isMatch = await bcrypt.compare(password, user.password_hash);
+        if (!isMatch) {
+            return res.status(404).json({ message: 'Invalid credentials' });
+        }
+
+        //Generate Token 
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+            expiresIn: '1d',
+        });
+
+        res.json({
+            message: 'Login successful',
+            token,
+            user: {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+}   
--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const { connectDB } = require('./config/database'); // Import DB connection
+const authRoutes = require('./routes/authRoutes'); // <--- IMPORT ROUTES
 
+require('dotenv').config();
 
 const app = express();
+
+// Connect to Database
+connectDB(); // <--- CALL DB CONNECTION
+
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+// Use Routes
+app.use('/api/auth', authRoutes); // <--- MOUNT ROUTES
+// This means any request starting with /api/auth will go to authRoutes
+// Example: /api/auth/register
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send("Pulse API Running")
+    res.send('Pulse API Running');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-
-})
+    console.log(`Server running on port ${PORT}`);
+});
