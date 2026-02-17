92e6a99ee70ad8e34244d35964683954e86111c3
2arnav4 <singlaarnav2405@gmail.com>
2026-02-17 15:09:07 +0530

    Initial Commit

---
+++ Backend/models/User.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    avatar_url: {
+        type: DataTypes.STRING,
+        defaultValue: 'https://via.placeholder.com/150',
+    },
+}, {
+    timestamps: true, // Adds createdAt and updatedAt automatically
+});
+
+module.exports = User;
